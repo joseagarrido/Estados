bb8319c233645fd44e7a3b5334c449f71817a05e
Jose Antonio Garrido Martinez <jgarrido@iMac-de-Jose.local>
2021-02-03 12:09:58 +0100

    diseño listo

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:estado/pages/pagina1_pages.dart';
+import 'package:estado/pages/pagina2_page.dart';
+import 'package:flutter/material.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: 'pagina1',
+      routes: {
+        'pagina1':(_) => Pagina1Page(),
+        'pagina2':(_) => Pagina2Page()
+      },
+    );
+  }
+}
---
+++ lib/models/usuario.dart
@@ -0,0 +1,10 @@
+
+class Usuario {
+
+  String nombre;
+  int edad;
+  List<String> profesiones;
+
+  Usuario({this.nombre, this.edad, this.profesiones});
+  
+}
